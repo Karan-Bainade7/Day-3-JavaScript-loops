e735de2b7663dc0228d403e34ae81cbc4a5d4ce2
karan punamsing bainade <119426472+Karan-Bainade7@users.noreply.github.com>
2024-11-24 22:04:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// for (i = 20; i <= 50; i++) {
+//     console.log(i);
+// }
+// console.log("Done");
+
+
+
+// let i = 0;
+// while (i <= 50) {
+//     console.log(i);
+//     i++;
+// }
+// console.log("Done");
+
+
+
+// //print even no.1-50
+// for (i = 1; i <= 50; i++) {
+
+//     if (i % 2 == 0) {
+//         console.log(i);
+
+//     }
+// }
+// console.log("Done");
+
+
+
+// //print odd no.1-50
+// for (i = 1; i <= 50; i++) {
+
+//     if (i % 2 !== 0) {
+//         console.log(i);
+
+//     }
+// }
+// console.log("Done");
+
+
+
+
+//gssing number 25
+
+
+let mynum = 25;
+let usernum = prompt("enter your no to match my number:");
+while (mynum !== usernum) {
+
+}
+console.log("congratulation you guessed the number");
